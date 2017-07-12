9c00f19b7bab5f3b92ead8012a022819b12652e1
jcmayoral <jocamaba1989@gmail.com>
2017-07-12 12:10:15 +0000

    accelerometer_ros node reading from sensor

---
+++ accelerometer_ros/setup.py
@@ -0,0 +1,13 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+d = generate_distutils_setup(
+    packages=['accelerometer_ros'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
+
