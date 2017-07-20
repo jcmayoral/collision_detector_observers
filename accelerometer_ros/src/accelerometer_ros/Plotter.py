010bbfd1512611cd8c3aab146e06c49795a1b6c2
jcmayoral <jocamaba1989@gmail.com>
2017-07-20 15:08:01 +0200

    plotting working but figure is not closing when node dies

--- accelerometer_ros/src/accelerometer_ros/Plotter.py
+++ accelerometer_ros/src/accelerometer_ros/Plotter.py
@@ -1,29 +1,37 @@
 import rospy
-import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import time
 from geometry_msgs.msg import AccelStamped
 
 class Plotter:
     def __init__(self, threshold = 1000):
         rospy.init_node("accelerometer_plotter", anonymous=True)
         self.data_ = []
-        rospy.Subscriber("accel", AccelStamped, self.accCB)
         self.step_ = []
         self.threshold_ = threshold
+        self.f, self.ax = plt.subplots()
+        self.ax.set_title('Simple plot')
+        rospy.Subscriber("accel", AccelStamped, self.accCB)
         print ("Plotter Constructor Initialized")
+        #self.anim_ = FuncAnimation(self._, self.data_, range(len(self.data_)), interval=10, repeat=False)
+        plt.show()
         rospy.spin()
+        rospy.on_shutdown(self.stop())
+
+    def stop(self):
+        plt.close("all")
 
     def accCB(self, msg):
         self.step_.append(msg.header.seq)
         self.data_.append([msg.accel.linear.x,msg.accel.linear.y, msg.accel.angular.z])
-        if (msg.header.seq % self.threshold_ is 0): 
-           #plt.plot(self.step_, self.data_)
-           self.f_ = plt.figure()
-           self.ax_ = self.f_.gca()
-           #plt.show()
-           self.ax_.plot(self.step_,self.data_)
-           self.f_.canvas.draw()
-           del self.step_[:]
-           del self.data_[:]
-           self.f_.show()
-
+        plt.draw()
+        if msg.header.seq % self.threshold_ is 0:
+            print ("Threshold reached")
+            self.ax.plot(self.step_, self.data_)
+        #self.ax_.plot(self.step_,self.data_)
+        #plt.draw()
+        #del self.step_[:]
+        #del self.data_[:]
+        #self.f_.show()
