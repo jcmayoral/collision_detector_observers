69a4c84a987ada93e2f2ae9cbab109ac6d8e6d68
jcmayoral <jocamaba1989@gmail.com>
2017-07-21 16:20:51 +0200

    cusum plotting but not working... perhaps other mean values

--- accelerometer_ros/src/fault_detection/CUSUM.py
+++ accelerometer_ros/src/fault_detection/CUSUM.py
@@ -1,6 +1,7 @@
 import rospy
 from FaultDetection import ChangeDetection
 from accelerometer_ros.RealTimePlotter import RealTimePlotter
+from geometry_msgs.msg import AccelStamped
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -9,7 +10,24 @@ class CUSUM(RealTimePlotter):
     def __init__(self, threshold = 1000, pace = 200, seconds=1):
         super().__init__(threshold,pace)
         rospy.init_node("accelerometer_cusum", anonymous=True)
-        self.changeDetection = ChangeDetection(seconds)
+        self.fd = ChangeDetection(seconds)
+        self.seq =[]
+        self.data =[]
+        self.count = 0
+        rospy.Subscriber("accel", AccelStamped, self.run)
         plt.show()
         rospy.spin()
         plt.close("all")
+
+    def run(self,msg):
+        self.fd.samples.append([msg.accel.linear.x,msg.accel.linear.y,msg.accel.angular.z])
+
+        if self.count is 1:
+            self.seq.append(msg.header.seq)
+            self.fd.changeDetection()
+            self.data.append(self.fd.cum_sum)
+            print (self.fd.cum_sum)
+            self.update(msg.header.seq,self.seq,self.data)
+            self.count = 0
+
+        self.count = self.count + 1
