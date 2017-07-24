aa5b3292130ef941269b9242d32b5f7584a7a6be
jcmayoral <jocamaba1989@gmail.com>
2017-07-24 12:01:30 +0200

    changes in organization folder

--- accelerometer_ros/src/fault_detection/AccCUSUM.py
+++ accelerometer_ros/src/fault_detection/AccCUSUM.py
@@ -8,20 +8,27 @@ import matplotlib.pyplot as plt
 
 class AccCUSUM(RealTimePlotter,ChangeDetection):
     def __init__(self, threshold = 1000, pace = 200):
-        self.data_ = []
+        self.data_ = [0,0,0]
         self.step_ = []
-        print ("Plotter Constructor Initialized")
+        self.i = 0
         RealTimePlotter.__init__(self,threshold,pace)
         ChangeDetection.__init__(self,10)
-        rospy.init_node("accelerometer_plotter", anonymous=True)
+        rospy.init_node("accelerometer_cusum", anonymous=True)
         rospy.Subscriber("accel", AccelStamped, self.accCB)
         plt.show()
         rospy.spin()
         plt.close("all")
 
     def accCB(self, msg):
-        self.addData([msg.accel.linear.x,msg.accel.linear.y, msg.accel.angular.z])
+        while (self.i<100):
+            self.addData([msg.accel.linear.x,msg.accel.linear.y, msg.accel.angular.z])
+            self.i = self.i+1
+            if len(self.samples) is self.threshold_:
+                self.samples.pop(0)
+            return
+        self.i=0
+        print (self.cum_sum)
         self.changeDetection()
         self.step_.append(msg.header.seq)
-        self.data_.append(self.cum_sum)
+        self.data_.append(self.cum_sum - self.data_[-1])
         self.update(msg.header.seq,self.step_,self.data_)
