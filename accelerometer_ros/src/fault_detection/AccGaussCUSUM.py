f0c5471dcd44434795c16fb874e84fff3054f0ee
jcmayoral <jocamaba1989@gmail.com>
2017-08-07 12:27:52 +0200

    implementing gauss bell plot

--- accelerometer_ros/src/fault_detection/AccGaussCUSUM.py
+++ accelerometer_ros/src/fault_detection/AccGaussCUSUM.py
@@ -1,14 +1,15 @@
 import rospy
 from MyStatics.RealTimePlotter import RealTimePlotter
+from MyStatics.GaussianPlotter import GaussPlot
+from FaultDetection import ChangeDetection
 from geometry_msgs.msg import AccelStamped
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
-from FaultDetection import ChangeDetection
 import matplotlib.pyplot as plt
 
 
-class AccCUSUM(RealTimePlotter,ChangeDetection):
+class AccGaussCUSUM(RealTimePlotter,ChangeDetection,GaussPlot):
     def __init__(self, max_samples = 500, pace = 2, cusum_window_size = 10 ):
         self.data_ = []
         self.data_.append([0,0,0])
@@ -19,6 +20,7 @@ class AccCUSUM(RealTimePlotter,ChangeDetection):
         self.window_size = cusum_window_size
         RealTimePlotter.__init__(self,max_samples,pace)
         ChangeDetection.__init__(self,10)
+        GaussPlot.__init__(self )
         rospy.init_node("accelerometer_cusum", anonymous=True)
         rospy.Subscriber("accel", AccelStamped, self.accCB)
         plt.legend()
@@ -36,7 +38,8 @@ class AccCUSUM(RealTimePlotter,ChangeDetection):
         self.i=0
         self.changeDetection(len(self.samples))
         cur = np.array(self.cum_sum, dtype = object)
+        self.call(self.s_z,np.mean(self.samples, axis=0),np.var(self.samples, axis=0))
         self.step_.append(self.msg)
         self.data_.append(cur)
         self.msg = self.msg + 1
-        self.update(msg.header.seq,self.step_,self.data_)
+        self.update(msg.header.seq,self.x,self.rv.pdf(self.x))
