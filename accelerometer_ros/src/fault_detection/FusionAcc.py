88d16df2c6062bd702a6aab75dc89ba8c41f173b
jcmayoral <jocamaba1989@gmail.com>
2017-09-06 12:58:52 +0200

    Filling message COllision with threshold

--- accelerometer_ros/src/fault_detection/FusionAcc.py
+++ accelerometer_ros/src/fault_detection/FusionAcc.py
@@ -5,12 +5,15 @@ from fusion_msgs.msg import sensorFusionMsg
 import numpy as np
 
 class FusionAcc(ChangeDetection):
-    def __init__(self, cusum_window_size = 10 ):
+    def __init__(self, cusum_window_size = 10, frame="base_link", sensor_id="accel1", threshold = 100):
         self.data_ = []
         self.data_.append([0,0,0])
         self.i = 0
         self.msg = 0
         self.window_size = cusum_window_size
+        self.frame = frame
+        self.sensor_id = sensor_id
+        self.threshold = threshold
         ChangeDetection.__init__(self,10)
         rospy.init_node("accelerometer_cusum", anonymous=True)
         rospy.Subscriber("accel", AccelStamped, self.accCB)
@@ -24,9 +27,22 @@ class FusionAcc(ChangeDetection):
             if len(self.samples) is self.window_size:
                 self.samples.pop(0)
             return
+
+        msg = sensorFusionMsg()
+
         self.i=0
         self.changeDetection(len(self.samples))
+
+
         cur = np.array(self.cum_sum, dtype = object)
-        msg = sensorFusionMsg()
+        #Filling Message
+        msg.header.frame_id = self.frame
+        msg.window_size = self.window_size
+        
+        #Detecting Collisions
+        if any(t > self.threshold for t in cur):
+            msg.msg = sensorFusionMsg.ERROR
+
+        msg.sensor_id.data = self.sensor_id
         msg.data = cur
         self.pub.publish(msg)
