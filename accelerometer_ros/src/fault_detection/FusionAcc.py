b368750fd57b3c63a3adefec3699af54fbb4ea95
jcmayoral <jocamaba1989@gmail.com>
2018-01-10 15:05:04 +0100

    orientation attempt number 1

--- accelerometer_ros/src/fault_detection/FusionAcc.py
+++ accelerometer_ros/src/fault_detection/FusionAcc.py
@@ -15,7 +15,6 @@ class FusionAcc(ChangeDetection):
     def __init__(self, cusum_window_size = 10, frame="base_link", sensor_id="accel1", threshold = 60):
         self.data_ = []
         self.data_.append([0,0,0])
-        self.last_measure = [0,0,0]
         self.i = 0
         self.msg = 0
         self.window_size = cusum_window_size
@@ -37,7 +36,7 @@ class FusionAcc(ChangeDetection):
         return config
 
     def accCB(self, msg):
-        print (self.last_mean)
+
         while (self.i< self.window_size):
             self.addData([msg.accel.linear.x,msg.accel.linear.y, msg.accel.angular.z])
             self.i = self.i+1
@@ -47,24 +46,23 @@ class FusionAcc(ChangeDetection):
 
         output_msg = sensorFusionMsg()
 
+        current_mean = np.mean(self.samples, axis=0)
+        diff = np.round(np.array(current_mean) - np.array(self.last_mean))
+        #print (diff)
+        output_msg.angle = np.degrees(np.arctan2(diff[0],diff[1]))
+        #Detecting Collisions
+
         self.i=0
         self.changeDetection(len(self.samples))
-
 
         cur = np.array(self.cum_sum, dtype = object)
         #Filling Message
         output_msg.header.frame_id = self.frame
         output_msg.window_size = self.window_size
 
-        new_measure = [msg.accel.linear.x,msg.accel.linear.y, msg.accel.angular.z]
-        diff = (np.array(new_measure) - np.array(self.last_measure))
-        self.last_measure = new_measure
-        output_msg.angle = np.degrees(np.arctan2(diff[1],diff[0]))
-        #Detecting Collisions
         if any(t > self.threshold for t in cur):
             output_msg.msg = sensorFusionMsg.ERROR
-            print np.degrees(np.arctan2(diff[1],diff[0]))
-
+            print np.degrees(np.arctan2(diff[0],diff[1]))
 
 
         """
@@ -73,9 +71,9 @@ class FusionAcc(ChangeDetection):
         pca.fit(self.samples)
         pca = pca.transform(self.samples)
 
-        print ("New:")
         #print(pca.components_)
         print(pca.explained_variance_ratio_)
+        print ("New:")
         current_angle = np.degrees(np.arctan2(pca.explained_variance_ratio_[0],pca.explained_variance_ratio_[1]))
         #current_angle = np.degrees(np.arctan2(cur[1],cur[0]))
         msg.angle = current_angle - self.last_angle
