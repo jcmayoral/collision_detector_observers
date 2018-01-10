447aa8b823e4748b1d6f7be2f516d606ec215253
jcmayoral <jocamaba1989@gmail.com>
2018-01-10 13:33:08 +0100

    improvement for angle fusionacc

--- accelerometer_ros/src/fault_detection/FusionAcc.py
+++ accelerometer_ros/src/fault_detection/FusionAcc.py
@@ -4,6 +4,9 @@ from geometry_msgs.msg import AccelStamped
 from fusion_msgs.msg import sensorFusionMsg
 import numpy as np
 
+# For PCA
+from sklearn.decomposition import PCA
+
 #Dynamic Reconfigure
 from dynamic_reconfigure.server import Server
 from accelerometer_ros.cfg import accelerometerConfig
@@ -12,6 +15,7 @@ class FusionAcc(ChangeDetection):
     def __init__(self, cusum_window_size = 10, frame="base_link", sensor_id="accel1", threshold = 60):
         self.data_ = []
         self.data_.append([0,0,0])
+        self.last_measure = [0,0,0]
         self.i = 0
         self.msg = 0
         self.window_size = cusum_window_size
@@ -33,6 +37,7 @@ class FusionAcc(ChangeDetection):
         return config
 
     def accCB(self, msg):
+        print (self.last_mean)
         while (self.i< self.window_size):
             self.addData([msg.accel.linear.x,msg.accel.linear.y, msg.accel.angular.z])
             self.i = self.i+1
@@ -40,7 +45,7 @@ class FusionAcc(ChangeDetection):
                 self.samples.pop(0)
             return
 
-        msg = sensorFusionMsg()
+        output_msg = sensorFusionMsg()
 
         self.i=0
         self.changeDetection(len(self.samples))
@@ -48,15 +53,40 @@ class FusionAcc(ChangeDetection):
 
         cur = np.array(self.cum_sum, dtype = object)
         #Filling Message
-        msg.header.frame_id = self.frame
-        msg.window_size = self.window_size
+        output_msg.header.frame_id = self.frame
+        output_msg.window_size = self.window_size
 
+        new_measure = [msg.accel.linear.x,msg.accel.linear.y, msg.accel.angular.z]
+        diff = (np.array(new_measure) - np.array(self.last_measure))
+        self.last_measure = new_measure
+        output_msg.angle = np.degrees(np.arctan2(diff[1],diff[0]))
         #Detecting Collisions
         if any(t > self.threshold for t in cur):
-            msg.msg = sensorFusionMsg.ERROR
+            output_msg.msg = sensorFusionMsg.ERROR
+            print np.degrees(np.arctan2(diff[1],diff[0]))
 
-        msg.header.stamp = rospy.Time.now()
-        msg.sensor_id.data = self.sensor_id
-        msg.data = cur
-        msg.weight = self.weight
-        self.pub.publish(msg)
+
+
+        """
+        #TODO FOR PCA
+        pca = PCA(n_components=2)
+        pca.fit(self.samples)
+        pca = pca.transform(self.samples)
+
+        print ("New:")
+        #print(pca.components_)
+        print(pca.explained_variance_ratio_)
+        current_angle = np.degrees(np.arctan2(pca.explained_variance_ratio_[0],pca.explained_variance_ratio_[1]))
+        #current_angle = np.degrees(np.arctan2(cur[1],cur[0]))
+        msg.angle = current_angle - self.last_angle
+        self.last_angle = current_angle
+
+        print (pca.explained_variance_)
+        print(np.arctan2(pca.explained_variance_[1],pca.explained_variance_[0]))
+        print(pca.explained_variance_)
+        """
+        output_msg.header.stamp = rospy.Time.now()
+        output_msg.sensor_id.data = self.sensor_id
+        output_msg.data = cur
+        output_msg.weight = self.weight
+        self.pub.publish(output_msg)
