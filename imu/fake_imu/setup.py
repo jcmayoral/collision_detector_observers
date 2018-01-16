d2f653645a3a087430086d88d8e16525cc37eefa
jcmayoral <jocamaba1989@gmail.com>
2018-01-16 15:15:13 +0100

    imu_fake_detection removed from fake_imu

--- imu/fake_imu/setup.py
+++ imu/fake_imu/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 d = generate_distutils_setup(
-    packages=['fake_imu', 'imu_fault_detection'],
+    packages=['fake_imu'],
     package_dir={'': 'src'}
 )
 
